65854b432e5ece3facefb1162d83816d164aa791
Chantelle S <chantelle.shum@tum.de>
2024-10-15 16:32:58 +0200

    Added comparison for 1850 and 2019 polar circle temperatures

---
+++ compare_script.m
@@ -0,0 +1,49 @@
+l850_path = "./sea_surface_temperature/1850/";
+l019_path = "./sea_surface_temperature/2019/";
+
+l850_S = dir(fullfile(l850_path, "*.csv"));
+l019_S = dir(fullfile(l019_path, "*.csv"));
+
+% extract data from 1850
+% https://de.mathworks.com/matlabcentral/answers/2035849-loading-multiple-csv-files-using-readmatrix
+for k = 1:numel(l850_S)
+    F = fullfile(l850_path, l850_S(k).name);
+    l850_S(k).data = readmatrix(F);
+end
+
+% extract data from 2019
+for k = 1:numel(l019_S)
+    F = fullfile(l019_path, l019_S(k).name);
+    l019_S(k).data = readmatrix(F);
+end
+
+l850_polar_mean = [];
+for k = 1:12
+    polar = [l850_S(k).data(1:24, :); l850_S(k).data(156:180, :)];
+    polar = (polar < 1e9).*polar;
+    curr_mean = mean(polar, "all");
+    l850_polar_mean = [l850_polar_mean, curr_mean];
+end
+
+l019_polar_mean = [];
+for k = 1:12
+    polar = [l019_S(k).data(1:24, :); l019_S(k).data(156:180, :)];
+    polar = (polar < 1e9).*polar;
+    curr_mean = mean(polar, "all");
+    l019_polar_mean = [l019_polar_mean, curr_mean];
+end
+
+mean_diff = l019_polar_mean - l850_polar_mean;
+
+f = figure(1);
+f.Position = [100 100 1000 800];
+% account for alphabetical order during extraction
+x = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+Y = [mean_diff(1), mean_diff(5:12), mean_diff(2:4)];
+bar(x, Y);
+text(1:length(Y),Y,num2str(Y', 3),'vert','bottom','horiz','center'); 
+title("1850 vs 2019 Polar Circle Temperature Difference")
+xlabel("Month")
+ylabel("Temperature (°C)")
+
+
