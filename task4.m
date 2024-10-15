8a31e602fdd9debee400b060ba7be5bbaf58ee34
Chih-Ting Chen <go36qad@mytum.de>
2024-10-15 22:10:12 +0200

    Matlab code of task 4- polar circle average temp comparison for 1850 and 2019

---
+++ task4.m
@@ -0,0 +1,18 @@
+temp_avg=zeros(1,12)
+for (k=1:12)
+    route_1950=sprintf("/Users/chenzhiting/Desktop/1850/%d.csv",k)
+    route_2019=sprintf("/Users/chenzhiting/Desktop/2019/%d.csv",k)
+
+    Data_1950_1=csvread(route_1950)
+    Data_2019_1=csvread(route_2019)
+    Data_1950_1=(Data_1950_1 < 1e4).*Data_1950_1
+    Data_2019_1=(Data_2019_1 < 1e4).*Data_2019_1
+    polar_1950_1=Data_1950_1([1:25,157:180],:)
+    avg_polar_1950_1=mean(polar_1950_1(:))
+    polar_2019_1=Data_2019_1([1:25,157:180],:)
+    avg_polar_2019_1=mean(polar_2019_1(:))
+    diff=abs(avg_polar_1950_1-avg_polar_2019_1)
+    temp_avg(1,k)=diff
+end
+
+plot(temp_avg)
