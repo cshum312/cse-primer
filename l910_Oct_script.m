3519c8c534ae920a131967519c35a0c5bb381de6
Chantelle S <chantelle.shum@tum.de>
2024-10-15 17:12:36 +0200

    edited for matrix multiplication and file path

--- l910_Oct_script.m
+++ l910_Oct_script.m
@@ -1,5 +1,5 @@
-D_1910=csvread("/Users/chenzhiting/Desktop/1910/10.csv")
-data_fil=(D_1910 < 1e9)*D_1910
+D_1910=csvread("./sea_surface_temperature/1910/10.csv")
+data_fil=(D_1910 < 1e9).*D_1910
 imagesc(data_fil)
 colorbar
 colormap(jet)
