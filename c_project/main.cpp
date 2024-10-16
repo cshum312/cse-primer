f40c208207e7f2c5f24ae0a8d62ef0648e812a68
Chantelle S <chantelle.shum@tum.de>
2024-10-16 14:19:46 +0200

    testing hpp

--- c_project/main.cpp
+++ c_project/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "csv_reader.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<std::vector<double>> v;
+    v = read_csv("../sea_ice_concentration/data/1850/1.csv");
+
+    for (auto vec : v) {
+        for (auto row : vec) {
+            std::cout << row << std::endl;
+        }
+    }
+
     return 0;
 }
